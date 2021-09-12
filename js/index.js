6d5d07cd13b56aac785b27bc2c83fab386e22e22
azavoloko <azavoloko@seatgeek.com>
2021-09-12 09:35:40 +0300

    update

--- js/index.js
+++ js/index.js
@@ -45,12 +45,8 @@ $(document).ready(function () {
   function displayResults(response) {
     for (let i = 0; i < response.items.length; i += 2) {
       item = response.items[i];
-
       title1 = item.volumeInfo.title;
       description1 = item.volumeInfo.description;
-      author1 = item.volumeInfo.authors;
-      publisher1 = item.volumeInfo.publisher;
-      bookLink1 = item.volumeInfo.previewLink;
       bookIsbn = item.volumeInfo.industryIdentifiers[0].identifier;
       bookImg1 = item.volumeInfo.imageLinks
         ? item.volumeInfo.imageLinks.thumbnail
@@ -58,12 +54,8 @@ $(document).ready(function () {
 
       item2 = response.items[i + 1];
       title2 = item2.volumeInfo.title;
-
       description2 = item2.volumeInfo.description;
       console.log(description2);
-      author2 = item2.volumeInfo.authors;
-      publisher2 = item2.volumeInfo.publisher;
-      bookLink2 = item2.volumeInfo.previewLink;
       bookIsbn2 = item2.volumeInfo.industryIdentifiers[0].identifier;
       bookImg2 = item2.volumeInfo.imageLinks
         ? item2.volumeInfo.imageLinks.thumbnail
@@ -71,8 +63,8 @@ $(document).ready(function () {
 
       outputList.innerHTML +=
         '<div class="row mt-4">' +
-        formatOutput(bookImg1, title1, description1, bookLink1, bookIsbn) +
-        formatOutput(bookImg2, title2, description2, bookLink2, bookIsbn2) +
+        formatOutput(bookImg1, title1, description1, bookIsbn) +
+        formatOutput(bookImg2, title2, description2, bookIsbn2) +
         "</div>";
     }
   }
@@ -82,7 +74,7 @@ $(document).ready(function () {
    * @param bookImg title author publisher bookLink
    * @return htmlCard
    */
-  function formatOutput(bookImg, title, bookIsbn, description) {
+  function formatOutput(bookImg, title, description, bookIsbn) {
     // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
     let viewUrl = "book.html?isbn=" + bookIsbn; //constructing link for bookviewer
     let htmlCard = `<div class="col-lg-6">
